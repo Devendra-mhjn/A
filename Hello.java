0437a5688e0c710977580661842ffc0e936bc771
Devendra-mhjn <84124763+Devendra-mhjn@users.noreply.github.com>
2021-10-07 15:50:14 +0530

    udpate hello

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello
 {
     public static int a=5;
+    String name ="Devendra";
    public static void main(String args[])
    {
      System.out.println("Hello Git merge conflict in Master Mege c");
