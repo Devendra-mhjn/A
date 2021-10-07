a2d34f97db02d51f3767376569085e25d63baaf3
Devendra-mhjn <devendra_mhjn@rediffmail.com>
2021-10-07 14:58:35 +0530

    commit detail by repor C

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 class Hello
 {
+    public static int a=5;
    public static void main(String args[])
    {
-     System.out.println("Hello Git merge conflict in Master");
+     System.out.println("Hello Git merge conflict in Master Mege c");
      }
 }     
   
